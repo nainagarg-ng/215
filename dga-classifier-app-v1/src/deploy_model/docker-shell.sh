4dddf227d1d4d2573481ce2deb1cdb01a8cb6a6b
daniel-more <daniel.more.torres@outlook.com>
2023-12-08 15:38:48 -0500

    reorganize repo

--- dga-classifier-app-v1/src/deploy_model/docker-shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Build the image based on the Dockerfile
-docker build -t dga-deployed -f Dockerfile .
-
-# Run Container
-docker run --rm -it -p 7080:7080 -p 7081:7081  --platform=linux/amd64 --name dga-deployed dga-deployed
