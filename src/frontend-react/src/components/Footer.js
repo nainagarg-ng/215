1b035e38ed34b73be1d1e330dfd3fad4228465c2
daniel-more <daniel.more.torres@outlook.com>
2023-12-08 16:27:27 -0500

    /run-deploy-app

--- src/frontend-react/src/components/Footer.js
+++ src/frontend-react/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer style={{ textAlign: 'center', marginTop: '50px', padding: '20px', borderTop: '1px solid #eaeaea' }}>
       <Typography variant="body2" color="textSecondary">
-        &copy; 2023 DGA Classifier. All rights reserved.
+        &copy; 2023 DGA Classifier. All rights reserved. version 3
       </Typography>
     </footer>
   );
