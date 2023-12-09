1a0eb408140350940ef9ba123a14806e30238211
daniel-more <daniel.more.torres@outlook.com>
2023-12-08 21:59:15 -0500

    /run-deploy-app

--- src/deployment/deploy-app.sh
+++ src/deployment/deploy-app.sh
@@ -1,2 +1,2 @@
 ansible-playbook deploy-docker-images.yml -i inventory.yml
-ansible-playbook update-k8s-cluster.yml -i inventory-prod.yml
+ansible-playbook update-k8s-cluster.yml -i inventory-prod.yml --extra-vars cluster_state=present
